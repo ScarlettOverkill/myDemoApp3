543ca51f02bb9f1652b49727d3a4a9afa0e9a3d6
Selen <syel@etu.edu.tr>
2017-02-02 16:38:05 +0200

    Hola first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.app;
+import java.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
